95b94b6b86d9780449bbc54aa8ba4216d5b3e479
arfan36 <arfanur36@gmail.com>
2022-09-24 23:18:44 +0600

    finish

--- src/components/Dial/Dial.js
+++ src/components/Dial/Dial.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Tablet from '../Tablet/Tablet';
 
 const Dial = (props) => {
     return (
         <div style={{border: "2px solid blue", margin: "5px"}}>
             <h3>This is Dial</h3>
             <p>steps so far: {props.steps}</p>
+            <Tablet></Tablet>
         </div>
     );
 };
---
+++ src/components/Tablet/Tablet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Tablet extends Component {
+    render() {
+        return (
+            <div style={{border:"2px solid green", margin: "5px"}}>
+                <h3>This is Tablet</h3>
+            </div>
+        );
+    }
+}
+
+export default Tablet;
